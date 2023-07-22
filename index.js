af413b846a621b82c6e459891c771e4acc6574d2
bryan athallah <bryanathallah7@gmail.com>
2023-07-22 19:44:10 +0700

    bismillah works

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    const data = process.env.CYCLIC_DB;
-    res.send('WUZZUPPPPP!' + data)
-})
-app.listen(process.env.PORT || 3000)
--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const io = new Server(server, {
   cors: {
     origin: process.env.END_POINT,
     methods: ["GET", "POST"],
+    credentials: true,
+    transports: ["polling"],
   },
 });
 
