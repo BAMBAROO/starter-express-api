02de17349817ae194b7e22d597200a6e3afc7627
bryan athallah <bryanathallah7@gmail.com>
2023-09-17 16:32:29 +0700

    new bug fixing

--- controller/auth.js
+++ controller/auth.js
@@ -65,7 +65,7 @@ export const login = async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
-      secure: false,
+      secure: true,
     });
     res.status(200).json({ accessToken });
   } catch (err) {
